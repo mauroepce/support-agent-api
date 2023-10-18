335ccabce4fe2d2e7c41302d7970eb02701d84ad
mauroepce <mauricioeslainerpc@gmail.com>
2023-10-17 22:10:35 -0300

    .eslintrc & .prettierrc config

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -15,11 +15,15 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'warn', // Warns when 'console' statements are present
+    'eqeqeq': 'error', // Requires using '===' instead of '=='
+    'curly': 'error', // Requires curly braces for all control statements
+    'semi': ['error', 'always'], // Requires semicolons
   },
 };
