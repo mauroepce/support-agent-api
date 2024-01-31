183acba3e9b5ae1e359568828d3731498aa95df9
Daniel9822 <dhdany64@gmail.com>
2024-01-30 22:04:19 -0400

    update: se agrego un manejador de errores

--- src/server.js
+++ src/server.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const path = require('path');
-const { main } = require('./supportChat');
-const app = express();
+const express = require('express')
+const path = require('path')
+const { main } = require('./supportChat')
+const app = express()
 
 // Sirve los archivos estáticos desde la carpeta 'src'
-app.use(express.static(path.join(__dirname, '.')));
-app.use(express.json()); // Middleware to parse json
+app.use(express.static(path.join(__dirname, '.')))
+app.use(express.json()) // Middleware to parse json
 
-app.post('/chat', async (req,res) => {
-    const userQuery = req.body.userQuery
-    const response = await main(userQuery)
-    res.json({ reply: response })
+app.post('/chat', async (req, res) => {
+  const userQuery = req.body.userQuery
+  const response = await main(userQuery)
+  res.json({ reply: response })
 })
 
 // Ruta principal que sirve index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
-const port = process.env.PORT || 3000;
+// Manejador de errores para las funciones asincronas
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).send({
+    error: true,
+    message: err?.message
+  })
+})
+
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+  console.log(`Servidor escuchando en http://localhost:${port}`)
+})
