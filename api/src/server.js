7b3e60d18cc29ee1b19ae14f943208b69d9e2b13
Daniel9822 <dhdany64@gmail.com>
2024-02-22 17:46:26 -0400

    refactor: create api and client

--- api/src/server.js
+++ api/src/server.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`)
 })
